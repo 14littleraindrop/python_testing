ef373b14a699d5a2ee75411a36b84fc4c91b3054
Helen Chen <helen86@uw.edu>
2019-08-21 11:23:04 -0700

    start

---
+++ mean.py
@@ -0,0 +1,10 @@
+def mean(num_list):
+    try:
+        return sum(num_list)/len(num_list)
+    except ZeroDivisionError :
+        return 0
+    except TypeError as detail :
+        msg = "The algebraic mean of an non-numerical list is undefined.\
+               Please provide a list of numbers."
+        print ("test")
+        raise TypeError(detail.__str__() + "\n" +  msg)
